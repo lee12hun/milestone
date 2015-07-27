ef47c1747f8a357e23bdd3a3fa8ab1fc49994e60
lee12hun <lee12hun@hotmail.com>
2015-07-27 18:21:07 +0900

    express  mongo db 연결

--- CarTeamSource/serverfile/source/routes/index.js
+++ CarTeamSource/serverfile/source/routes/index.js
@@ -1,14 +1,105 @@
 var express = require('express');
-var database = require('./innerdata/database');
-
+var db = require('./innerdata/database');
 var router = express.Router();
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.post('/signup', function(req, res, next) {
+
+	console.log(req.body);
+	//console.log(db.Database.collections);
+
+	///////////////////////////////////////////////////////////////////////
+	// save or create
+
+	db.SignUp.create( req.body , function( err, item )
+	{	
+		if( err ){
+			console.log('error create = ', err );
+			//return handleError(err);
+		}
+		else{
+			console.log('create sucess ',item );
+			//res.end(JSON.stringify());
+			res.writeHead(200,{'Content-Type':'application/json'});
+			res.end(JSON.stringify({'Success ITEM':item}));
+		}
+	});
+
+	// var person = new db.SignUp( signup );
+
+	// person.save( function( err ) {
+	// 	if( err ){
+	// 		console.log('error save = ' + err );
+	// 	}
+	// 	else{
+	// 		// SignUpCollection.find({})
+	// 		// .populate('user_id')
+	// 		// .populate('test_comments.postedBy')
+	// 		// .exec(function(err,item){
+	// 		// 	console.log( JSON.stringify(item, null, "\t"));
+	// 		// });
+	// 	}
+	// });
+
+});
+
+
+router.post('/pageWrite', function(req, res, next) {
+
+	console.log(req.body);
+	//console.log(db.Database.collections);
+
+	///////////////////////////////////////////////////////////////////////
+	// save or create
+
+	console.log("db.PageList" , db.PageList);
+
+	db.PageList.create( req.body , function( err, item )
+	{	
+		if( err ){
+			console.log('error create = ', err );
+			//return handleError(err);
+		}
+		else{
+			console.log('create sucess ',item );
+			//res.end(JSON.stringify());
+			res.writeHead(200,{'Content-Type':'application/json'});
+			res.end(JSON.stringify({'Success ITEM':item}));
+		}
+	});
+});
+
+
+router.post('/replay/insert', function(req, res, next) {
+
+	console.log(req.body);
+	//console.log(db.Database.collections);
+
+	///////////////////////////////////////////////////////////////////////
+	// save or create
+
+	db.PageList.find( req.body.user_id )
+
+	db.PageList.create( req.body , function( err, item )
+	{	
+		if( err ){
+			console.log('error create = ', err );
+			//return handleError(err);
+		}
+		else{
+			console.log('create sucess ',item );
+			//res.end(JSON.stringify());
+			res.writeHead(200,{'Content-Type':'application/json'});
+			res.end(JSON.stringify({'Success ITEM':item}));
+		}
+	});
 });
 
 module.exports = router;
+
+
--- CarTeamSource/serverfile/source/routes/innerdata/database.js
+++ CarTeamSource/serverfile/source/routes/innerdata/database.js
@@ -1,39 +1,72 @@
 
+
+
+'use strict';
+
+//var mongoose = require("mongoose");
+//var config = require("express-config");
+  
+
+
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/carDB');
 
+var db = mongoose.connection;
 
-var Cat = mongoose.model("User", {name:String});
+db.on('error', console.error.bind( console, 'connection Error:' ));
 
-var kitty = new Cat({name:"kitty"});
-
-kitty.save(function(err){
-	if(err)		throw err;
-	else{
-		console.log('저장 성공!')
-	}
+db.once('open', function(callback){
+	console.log(callback);
+	console.log( "connection successful....");
 });
 
 
-// var options = {
-//   db: { native_parser: true },
-//   server: { poolSize: 5 },
-//   replset: { rs_name: 'myReplicaSetName' },
-//   user: 'lee12hun',
-//   pass: '1234'
-// }
-// mongoose.connect("mongodb://locahost/test", option);
+// test 
+exports.Database = db;
+
+
+///////////////////////////////////////////////////////////////////////
+// Regist Collection  Schema
+
+var Schema = mongoose.Schema;
+
+//unique_id:Schema.Types.ObjectId,
+var SIGNUP = mongoose.Schema({	
+	unique_id:Schema.Types.ObjectId,
+	user_id:String,
+	user_pw:String,
+	car_name:String,
+	car_birth:String,
+	updated:{type:Date, default: Date.now }	
+});
+
+//var SignUpCollection = mongoose.model('SignUp', SignUp, 'SignUp');
+exports.SignUp = mongoose.model('SignUp', SIGNUP, 'SignUp');
+
+
+var PAGELIST = mongoose.Schema({
+	unique_id:Schema.Types.ObjectId,
+	user_id:String,
+	title:String,
+	contents:String,
+	img_path:String,
+	replylist:[]
+}, {collection:"PageList"});
+
+//var PageListCollection = mongoose.model('PageList', SignUp);
+exports.PageList = mongoose.model('PageList', PAGELIST);
+
+var ReplyData = {
+	unique_id:Schema.Types.ObjectId,
+	user_id:String,
+	contents:String,
+	updated:{type:Date, default: Date.now }	
+};
+
+exports.Reply = ReplyData;
 
 
 
 
-//var mongo = mongoose.mongo;
 
-
-
-//var db = mongo.connection;
-
-
-
-//var datastore = require('./datastore.js');
